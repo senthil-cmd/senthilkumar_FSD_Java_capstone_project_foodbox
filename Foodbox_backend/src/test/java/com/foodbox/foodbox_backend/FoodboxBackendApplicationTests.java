fe6da4a400b08868d58fd2df10a34cc2aadc5ffc
Senthilkumar <34963570+senthil-cmd@users.noreply.github.com>
2021-08-12 14:38:56 +0530

    new commit

---
+++ Foodbox_backend/src/test/java/com/foodbox/foodbox_backend/FoodboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodbox.foodbox_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodboxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
