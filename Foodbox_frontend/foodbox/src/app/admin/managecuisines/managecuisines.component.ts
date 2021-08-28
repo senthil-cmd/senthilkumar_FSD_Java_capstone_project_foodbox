9f478277a9e038b57642145f27578d75113e9d9e
Senthilkumar <34963570+senthil-cmd@users.noreply.github.com>
2021-08-28 09:12:13 +0530

    added components

---
+++ Foodbox_frontend/foodbox/src/app/admin/managecuisines/managecuisines.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-managecuisines',
+  templateUrl: './managecuisines.component.html',
+  styleUrls: ['./managecuisines.component.css']
+})
+export class ManagecuisinesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Foodbox_frontend/foodbox/src/app/app.module.ts
+++ Foodbox_frontend/foodbox/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ManageproductComponent } from './admin/manageproduct/manageproduct.component';
+import { AddproductComponent } from './admin/addproduct/addproduct.component';
+import { ManagecuisinesComponent } from './admin/managecuisines/managecuisines.component';
+import { ManageuserComponent } from './admin/manageuser/manageuser.component';
+import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ManageproductComponent,
+    AddproductComponent,
+    ManagecuisinesComponent,
+    ManageuserComponent,
+    HeaderComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
